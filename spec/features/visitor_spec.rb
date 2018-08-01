c2f8b3916e0d437cc22a5b1977f3f936bedd80a2
Emmie Hayes <emmiehayes2@gmail.com>
2018-08-01 09:43:07 -0600

    user story 4 complete

--- spec/features/visitor_spec.rb
+++ spec/features/visitor_spec.rb
@@ -4,7 +4,7 @@ describe "Visitor visits awards index" do
   it "sees awards that link to award show pages" do
     award_1 = Award.create(name: 'Best')
     award_2 = Award.create(name: 'Worst')
-    
+
     visit awards_path
 
     expect(page).to have_content('Best')
@@ -14,5 +14,14 @@ describe "Visitor visits awards index" do
     expect(current_path).to eq(award_path(award_1))
     expect(page).to have_content('Best')
     expect(page).to_not have_content('Worst')
+  end
+
+  it 'cannot see the create new award form for admin only' do
+    award_1 = Award.create(name: 'Best')
+    award_2 = Award.create(name: 'Worst')
+
+    visit awards_path
+
+    expect(page).to_not have_content('Create A New Award')
   end
 end
