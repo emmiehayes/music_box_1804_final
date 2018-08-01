1182044cd490c3acab1267fd677b6966b38aa4e0
Emmie Hayes <emmiehayes2@gmail.com>
2018-08-01 09:18:36 -0600

    first feature test

--- spec/features/visitor_spec.rb
+++ spec/features/visitor_spec.rb
@@ -1,9 +1,18 @@
-# User Story #1
-#
-# As a Visitor,
-#   When I visit the awards index,
-#     I see all awards
-#       and each award should be a link to that award's show page.
-#
-# Testing requirements:
-# - users should see at least 2 awards listed on the page
+require 'rails_helper'
+
+describe "Visitor visits awards index" do
+  it "sees awards that link to award show pages" do
+    award_1 = Award.create(name: 'Best')
+    award_2 = Award.create(name: 'Worst')
+    
+    visit awards_path
+
+    expect(page).to have_content('Best')
+    expect(page).to have_content('Worst')
+
+    click_link 'Best'
+    expect(current_path).to eq(award_path(award_1))
+    expect(page).to have_content('Best')
+    expect(page).to_not have_content('Worst')
+  end
+end
