934bbf02ef01d61844831059d87d4a5307f24a37
Emmie Hayes <emmiehayes2@gmail.com>
2018-08-01 10:35:51 -0600

    complete user story 5

--- spec/features/visitor_spec.rb
+++ spec/features/visitor_spec.rb
@@ -31,20 +31,21 @@ describe "Visitor visits awards index" do
     song_1 = artist_1.songs.create(title: "Don't Stop Believing", length: 320, play_count: 390808)
     song_2 = artist_1.songs.create(title: "Another Song", length: 320, play_count: 390808)
 
-    award_1 = song_1.awards.create(name: 'Best')
-    award_2 = song_1.awards.create(name: 'Worst')
-    award_3 = song_2.awards.create(name: 'Best Artist')
-    award_4 = song_2.awards.create(name: 'Most Annoying')
+    award_1 = Award.create(name: 'Best')
+    award_2 = Award.create(name: 'Worst')
+    award_3 = Award.create(name: 'Best Artist')
+    award_4 = Award.create(name: 'Most Annoying')
 
-    sa1= SongAward.create(song: song_1, award: award_1, year: 1997)
+    sa1 = SongAward.create(song: song_1, award: award_1, year: 1997)
     sa2 = SongAward.create(song: song_1, award: award_2, year: 1998)
     sa3 = SongAward.create(song: song_2, award: award_3, year: 1992)
     sa4 = SongAward.create(song: song_2, award: award_4, year: 1993)
 
     visit songs_path
-save_and_open_page
+
     expect(page).to have_content(song_1.title)
     expect(page).to have_content(song_2.title)
-    expect(page).to have_content("Awards for this Song\n#{award_1.name} in '#{sa1.year}',\n#{award_1.name} in #{sa2.year}")
+    expect(page).to have_content("Awards for this Song\n#{award_1.name} in #{sa1.year} #{award_2.name} in #{sa2.year}")
+    expect(page).to have_content("Awards for this Song\n#{award_3.name} in #{sa3.year} #{award_4.name} in #{sa4.year}")
   end
 end
