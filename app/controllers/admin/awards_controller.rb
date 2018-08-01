4fb3473c5550ff85a51ac8af502b0dc1111a82e6
Emmie Hayes <emmiehayes2@gmail.com>
2018-08-01 09:40:37 -0600

    complete user story 3

--- app/controllers/admin/awards_controller.rb
+++ app/controllers/admin/awards_controller.rb
@@ -1,7 +1,23 @@
-class Admin::CategoriesController < Admin::BaseController
+class Admin::AwardsController < Admin::BaseController
 
    def new
      @award = Award.new
    end
 
+  def create
+    @award = Award.new(award_params)
+    if @award.save
+      session[:award_id] = @award.id
+      redirect_to awards_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def award_params
+    params.require(:award).permit(:name)
+  end
 end
--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -16,4 +16,25 @@ describe "Admin visits awards index" do
     expect(page).to have_content('Name')
     expect(page).to have_button('Create Award')
   end
+
+  it "can fill out form to create new award" do
+    admin = User.create(username: "Dee", password: 'password', role: 1)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    award_1 = Award.create(name: 'Best')
+    award_2 = Award.create(name: 'Worst')
+    name = 'Best Artist'
+
+    visit awards_path
+    fill_in 'Name', with: name
+    click_button 'Create Award'
+
+    expect(current_path).to eq(awards_path)
+    expect(page).to have_content('Best')
+    expect(page).to have_content('Worst')
+    expect(page).to have_content('Create A New Award')
+    expect(page).to have_content('Name')
+    expect(page).to have_button('Create Award')
+    expect(page).to have_content('Best Artist')
+  end
 end
