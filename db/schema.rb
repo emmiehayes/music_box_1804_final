14604eb7cbef1425ae4b731b6b519ba940882917
Emmie Hayes <emmiehayes2@gmail.com>
2018-08-01 09:21:39 -0600

    pass model spec

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180724172333) do
+ActiveRecord::Schema.define(version: 20180801152056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 20180724172333) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "awards", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "playlist_songs", force: :cascade do |t|
