cc249a51aaa0cf4422e527ea4a1a6019aa2e629b
Emmie Hayes <emmiehayes2@gmail.com>
2018-08-01 09:51:55 -0600

    feature test for user story 5

--- spec/features/visitor_spec.rb
+++ spec/features/visitor_spec.rb
@@ -24,4 +24,27 @@ describe "Visitor visits awards index" do
 
     expect(page).to_not have_content('Create A New Award')
   end
+
+  it 'visits song index and sees awards for each song' do
+    artist_1 = Artist.create(name: 'Journey')
+
+    song_1 = artist_1.songs.create(title: "Don't Stop Believing", length: 320, play_count: 390808)
+    song_2 = artist_1.songs.create(title: "Another Song", length: 320, play_count: 390808)
+
+    award_1 = Award.create(name: 'Best')
+    award_2 = Award.create(name: 'Worst')
+    award_3 = Award.create(name: 'Best Artist')
+    award_4 = Award.create(name: 'Most Annoying')
+
+    SongAward.create(song: song_1, award: award_1)
+    SongAward.create(song: song_1, award: award_2)
+    SongAward.create(song: song_2, award: award_3)
+    SongAward.create(song: song_2, award: award_4)
+
+    visit songs_path
+
+    expect(page).to have_content(song_1.title)
+    expect(page).to have_content("Awards for #{song_1.title}:\n'Best' in '1997',\n'Worst in '1998'")
+    expect(page).to have_content("Awards for #{song_2.title}:\n'Best Artist' in '1992',\n'Most Annoying in '1993'")
+  end
 end
